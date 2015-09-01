147eba055887c8cb90f430a6717218ea56311e5c
Ari Selseng <ari@tbve.no>
2015-09-01 17:53:22 +0200

    - Fixed some bug with the hash test. - Removed trailing slashes. - Script should work ok now.

--- run.sh
+++ run.sh
@@ -5,34 +5,33 @@ scriptDir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 
 #source the function script
 source "$scriptDir"/functions.sh
-hash rsync lsof find xattr 2>/dev/null|| echo "Requirements: rsync lsof find xattr" && usage && exit 1 
+hash rsync lsof find xattr 2>/dev/null || echo "Requirements: rsync lsof find xattr"
 
-[-e "$scriptDir"/settings.conf ] && source "$scriptDir"/settings.conf||FINDOPTS=""
+[ -e "$scriptDir"/settings.conf ] && source "$scriptDir"/settings.conf||FINDOPTS=""
 
 
 while getopts "ht:a:b:" option
 do
      case $option in
          h)
-             usage
-             exit 1
+             usage && exit
              ;;
          t)
              minfind="$OPTARG"
              ;;
          a)
-             sourcedisk="$OPTARG"
+             sourcedisk="${OPTARG%/}"
              ;;
-		 b)
-             targetdisk="$OPTARG"
+	b)
+             targetdisk="${OPTARG%/}"
              ;;
 
          ?)
              usage
-             exit
+	    exit 1
              ;;
      esac
 done
 
-[ -z $sourcedisk ] && exit 1
+[ -z "$sourcedisk" ] && exit 1
 moveIdleFilesInPath "$sourcedisk" "$targetdisk"
